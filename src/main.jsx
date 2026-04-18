296bfe518dc87cccf51fcb8fc393ff84809083d9
SandeshMagar22 <sandeshmagar2229@gmail.com>
2026-04-18 12:46:50 +0545

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/yours-krishna/">
             <App />
         </BrowserRouter>
     </React.StrictMode>,
